7601a52ff3bcd81ef6fb4d37a9186e316ded15e7
Gideon Tettehfio <gideonlambride@gmail.com>
2024-05-03 22:35:27 +0000

    database setup

--- chatApp/chatApp_app/models.py
+++ chatApp/chatApp_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Message(models.Model):
+    text = models.TextField()
+    date_created = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.CharField(max_length=30)
+    room = models.CharField(max_length=50)
